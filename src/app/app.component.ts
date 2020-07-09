a7171bc197ec4ed965c51a9ee62c6af8e562c5fd
gauravahlawat81 <gauravahlawat81@gmail.com>
2020-07-10 00:16:33 +0530

    A barebone working UI app, at this stage, we are able to connect with backend and fetch the results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HttpHelperService} from './http-helper.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UI';
+
+  constructor(private httpService:HttpHelperService){}
+  serverData:any=null;
+  async getData(){
+    this.serverData = await this.httpService.getData();
+
+  }
 }
