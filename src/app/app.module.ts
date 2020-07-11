96a1996d01f32b825b1583eb75f72295fb273089
amujumdar <Ajit_Mujumdar@intuit.com>
2020-07-11 15:22:52 +0530

    Added filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http'
-
+import { HttpClientModule } from '@angular/common/http';
+import { SearchComponent } from './search/search.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { Datepicker2Component } from './datepicker2/datepicker2.component';
+import { TeachersearchComponent } from './teachersearch/teachersearch.component';
+import { OfficersearchComponent } from './officersearch/officersearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    DatepickerComponent,
+    Datepicker2Component,
+    TeachersearchComponent,
+    OfficersearchComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppMaterialModule
+    AppMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
