ef5d5c7399fd9bc6a91e5620b1ea07afe29a0f87
gauravahlawat81 <gauravahlawat81@gmail.com>
2020-07-30 15:11:41 +0530

    running out of funny messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { DbModel } from './shared/models/db.model';
 import { FetchDataService } from './fetch-data.service';
-import { Component , OnInit,ViewChild, ElementRef } from '@angular/core';
+import { Component , OnInit,ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import {HttpHelperService} from './http-helper.service'
 import { AngularFireDatabase } from '@angular/fire/database'
 import { ChartDataSets, ChartOptions } from 'chart.js';
@@ -14,7 +14,7 @@ import { Color, Label } from 'ng2-charts';
 export class AppComponent {
 
   title = 'UI'
-  items:any;
+  items:any=null;
   // school_address = 'School Address';
   wholeSchool: any
   constructor(private httpService:HttpHelperService,private fetchData:FetchDataService,private db: AngularFireDatabase){
